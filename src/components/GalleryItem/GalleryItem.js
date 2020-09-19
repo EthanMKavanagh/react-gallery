327d80053d3f2cc7e314a57e2b1d6f0f44fd0bd7
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-18 21:48:02 -0500

    conditional rendering in GalleryItem

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
 class GalleryItem extends Component {
+
+    state = {
+        isDescription: false
+    }
+
+    toggleDescription = () => {
+        console.log('Toggling description');
+        this.setState({
+            isDescription: !this.state.isDescription
+        });
+    }
+
     render() {
         return (
-            <div></div>
+            <div key={this.props.id}>
+                {
+                    this.state.isDescription ?
+                        <div>{this.props.description}</div> :
+                        <div>{this.props.path}</div>
+                        
+                }
+                <button onClick={this.toggleDescription}>
+                    {
+                        this.state.isDescription ?
+                            `Show Image` :
+                            `Show Description`
+                    }
+                </button>
+            </div>
         );
     }
 }
